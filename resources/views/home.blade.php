9349ea771e174cd03e378828ce96ad8f569fc3f7
Nick Peterson <npeterson1996@gmail.com>
2024-01-14 20:41:24 -0600

    updated nav bar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!-- Livewire Styles -->
+    @livewireStyles
+
+    <!-- Alpine.js -->
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.x.x/dist/alpine.min.js" defer></script>
+</head>
+<body>
+
+    <!-- Your Livewire Component -->
+    <p class="fire-engine-red">Hi</p>
+    <!-- Livewire Scripts -->
+    @livewireScripts
+</body>
+</html>
+</x-app-layout>
