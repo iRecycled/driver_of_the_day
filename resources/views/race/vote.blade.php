e1f0b9d16c87c85c2bb1a2eeb0b0a5412fc0a10b
Nick Peterson <npeterson1996@gmail.com>
2024-01-31 16:16:40 -0600

    updated votes page

--- resources/views/race/vote.blade.php
+++ resources/views/race/vote.blade.php
@@ -5,7 +5,6 @@
         <a class="bg-blue-500 text-white hover:text-blue-900 p-2 ml-6 rounded" href="/race/{{$race->session_id}}/results">View Results</a>
     </div>
     <div class="flex justify-center items-center p-6">
-        <p> {{ $race->session_id }}</p>
         @livewire('dotd-driver-vote', ['leagueId' => $race->league_id, 'seasonId' => $race->season_id, 'id' => $race->session_id])
     </div>
 </x-app-layout>
