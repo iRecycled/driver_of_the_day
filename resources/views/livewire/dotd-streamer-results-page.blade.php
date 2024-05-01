5259e70d616879ffad6afcabf5de99f184840010
Nick Peterson <npeterson1996@gmail.com>
2024-05-01 18:10:01 -0500

    removed if/else for zero check

--- resources/views/livewire/dotd-streamer-results-page.blade.php
+++ resources/views/livewire/dotd-streamer-results-page.blade.php
@@ -1,5 +1,6 @@
 <div>
     <div class="flex flex-col">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         <div wire:poll.3000ms></div>
         <div class="flex bg-black-500 justify-center">
             <h1>VOTE FOR DRIVER OF THE DAY</h1>
@@ -12,17 +13,10 @@
                 @php
                     $percentageVotes = ($totalVotes != 0) ? ($driver->getDriverVotes($race->id) / $totalVotes * 100) : 0;
                 @endphp
-                @if ($totalVotes == 0)
-                    <p class="flex flex-row justify-end text-black">0%</p>
-                    </span>
-                    <progress class="w-full h-6 text-black" value="0" max="100"></progress>
-                @else
                     <p class="flex flex-row justify-end text-black">{{ $percentageVotes }}%</p>
                     </span>
                     <progress class="w-full h-6 text-black" value={{ $percentageVotes }} max="100"></progress>
-                @endif
         </div>
         @endforeach
     </div>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </div>
