572e1411fe89f8d74ef4b14a3f871c9e935ae9a3
Nick Peterson <npeterson1996@gmail.com>
2024-07-25 18:19:39 -0500

    changed it to display a table instead of json

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -21,6 +21,43 @@ class ApiController extends Controller
                 'number' => $driver->car_number
             ];
         }
-        return $driversData;
+
+        $htmlTable = '
+        <style>
+            table {
+                width: 100%;
+                border-collapse: collapse;
+                margin: 20px 0;
+                font-size: 18px;
+                text-align: left;
+            }
+            th, td {
+                padding: 12px;
+                border-bottom: 1px solid #ddd;
+            }
+            th {
+                background-color: #f2f2f2;
+            }
+            tr:nth-child(even) {
+                background-color: #f9f9f9;
+            }
+            tr:hover {
+                background-color: #f1f1f1;
+            }
+        </style>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Number</th>
+            </tr>';
+
+        foreach ($driversData as $data) {
+            $htmlTable .= '<tr>';
+            $htmlTable .= '<td>' . htmlspecialchars($data['name']) . '</td>';
+            $htmlTable .= '<td>' . htmlspecialchars($data['number']) . '</td>';
+            $htmlTable .= '</tr>';
+        }
+        $htmlTable .= '</table>';
+        return $htmlTable;
     }
 }
